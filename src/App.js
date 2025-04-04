aee630c07c84a91ac21fa5aa7beda9c44cc1dbce
Vinay Gupta <109478270+iamrealvinnu@users.noreply.github.com>
2025-04-04 16:54:21 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,41 +1,51 @@
+
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CartProvider } from './components/CartContext';
 import Header from './components/Header';
 import Carousel from './components/Carousel';
-import Features from './components/Features';
 import PopularDishes from './components/PopularDishes';
 import DeliveryProcess from './components/DeliveryProcess';
+import Features from './components/Features';
+import HowItsWork from './components/HowItsWork';
 import Cart from './components/Cart';
-import { CartProvider } from './components/CartContext';
 import Menu from './pages/Menu';
+import Checkout from './pages/Checkout';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import Checkout from './pages/Checkout';
+import Chatbot from './components/Chatbot';
+import OrderTypeSelection from './pages/OrderTypeSelection';
 
 function App() {
   return (
-    <Router>
-      <CartProvider>
-        <div className="min-h-screen">
+    <CartProvider>
+      <Router>
+        <div className="App">
           <Header />
+          <Cart />
           <Routes>
-            <Route path="/" element={
-              <>
-                <Carousel />
-                <Features />
-                <PopularDishes />
-                <DeliveryProcess />
-              </>
-            } />
+            <Route
+              path="/"
+              element={
+                <>
+                  <Carousel />
+                  <PopularDishes />
+                  <DeliveryProcess />
+                  <Features />
+                  <HowItsWork />
+                </>
+              }
+            />
             <Route path="/menu" element={<Menu />} />
+            <Route path="/order-type" element={<OrderTypeSelection />} />
+            <Route path="/checkout" element={<Checkout />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/checkout" element={<Checkout />} />
           </Routes>
-          <Cart />
+          <Chatbot />
         </div>
-      </CartProvider>
-    </Router>
+      </Router>
+    </CartProvider>
   );
 }
 
